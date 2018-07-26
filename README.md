4a029777a57f40b9edaef2f64eb92c99eb9555e5
Rune Madsen <rune@runemadsen.com>
2018-07-26 16:20:12 +0200

    working for new lambda

--- README.md
+++ README.md
@@ -2,28 +2,17 @@
 
 A Lambda function for creating MailChimp subscriptions.
 
-##  Authentication
+## Setting up
 
-Set your MailChimp data center, API key, list ID, and username in the `.env` file. Copy the
-sample to get started:
+Set the environment variables in the `.env.sample` file as environment variables in your Lambda function.
 
-```
-$ cp .env.sample .env
-```
-
-Additional details about authenticating with the MailChimp API is available [here](http://developer.mailchimp.com/documentation/mailchimp/guides/get-started-with-mailchimp-api-3/).
-
-## Deployment
-
-There's a handy script included to create your zip archive:
+Then generate a `.zip` file for your lambda function:
 
 ```
 $ npm run zip
 ```
 
-Integrate with the
-[AWS API Gateway](http://docs.aws.amazon.com/lambda/latest/dg/gs-amazon-gateway-integration.html)
-to access the function via HTTP POST:
+Add a API Gateway in the Lambda UI, and test like this:
 
 ```
 $ curl -X POST -H "Content-Type: application/json" \
--- index.js
+++ index.js
@@ -1,75 +1,70 @@
-require('dotenv').load();
+var Promise = require("es6-promise").Promise,
+  request = require("superagent"),
+  md5 = require("md5");
 
-var Promise = require('es6-promise').Promise,
-    request = require('superagent'),
-    md5     = require('md5');
-
-var API_URL  = '.api.mailchimp.com/3.0/lists/',
-    DATACENTER  = process.env.DATACENTER,
-    API_KEY  = process.env.API_KEY,
-    LIST_ID  = process.env.LIST_ID,
-    USERNAME = process.env.USERNAME,
-    STATUS   = process.env.STATUS;
+var API_URL = ".api.mailchimp.com/3.0/lists/",
+  DATACENTER = process.env.DATACENTER,
+  API_KEY = process.env.API_KEY,
+  LIST_ID = process.env.LIST_ID,
+  USERNAME = process.env.USERNAME,
+  STATUS = process.env.STATUS;
 
 function urlForList() {
-  return 'https://' + DATACENTER + API_URL + LIST_ID + '/members/';
+  return "https://" + DATACENTER + API_URL + LIST_ID + "/members/";
 }
 
-function urlForUser(emailAddress) {
-  return urlForList() + md5(emailAddress);
+function urlForUser(email) {
+  return urlForList() + md5(email);
 }
 
-function updateSubscription(emailAddress) {
-  return new Promise(function(resolve, reject) {
-    request.patch(urlForUser(emailAddress))
-      .auth(USERNAME, API_KEY)
-      .send({ status: STATUS })
-      .end(function(err, res) {
-        if (err) {
-          console.log('ERROR', err);
-          reject({ status: err.status, message: err.response.text });
-        } else {
-          resolve(res.body);
-        }
-      });
-  });
-}
-
-function createSubscription(emailAddress) {
-  return new Promise(function(resolve, reject) {
-    request.post(urlForList())
-      .auth(USERNAME, API_KEY)
-      .send({ email_address: emailAddress, status: STATUS })
-      .end(function(err, res) {
-        if (err) {
-          console.log('ERROR', err);
-          reject({ status: err.status, message: err.response.text });
-        } else {
-          resolve(res.body);
-        }
-      });
-  });
-}
-
-exports.handler = function(event, context) {
-  var emailAddress = event.email;
-  function create() {
-    createSubscription(emailAddress)
-      .then(function(responseBody) {
-        context.succeed(responseBody);
-      })
-      .catch(function(err) {
-        context.fail(new Error(err));
-      });
+function createSubscription(email, name) {
+  var nameSplit = name ? name.split(" ") : [];
+  var merge_fields = {};
+  if (nameSplit[0]) {
+    merge_fields.FNAME = nameSplit[0];
   }
+  if (nameSplit[1]) {
+    merge_fields.LNAME = nameSplit[1];
+  }
+  return new Promise(function(resolve, reject) {
+    request
+      .post(urlForList())
+      .auth(USERNAME, API_KEY)
+      .set("Accept", "application/json")
+      .send({
+        email_address: email,
+        merge_fields: merge_fields,
+        status: STATUS
+      })
+      .end(function(err, res) {
+        if (err) {
+          reject({ status: err.status, message: err.response.text });
+        } else {
+          var returnRes = {
+            statusCode: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+              message: "Subscribed"
+            })
+          };
+          resolve(returnRes);
+        }
+      });
+  });
+}
 
-  updateSubscription(emailAddress)
-    .then(function(responseBody) {
-      context.succeed(responseBody);
+exports.handler = function(event, context, callback) {
+  var payload = JSON.parse(event.body);
+  console.log("Invoking Lambda with payload", payload);
+  createSubscription(payload.email, payload.name)
+    .then(function(res) {
+      console.log("MAILCHIMP SUCCESS", res);
+      callback(null, res);
     })
     .catch(function(err) {
-      if (err.status === 404) {
-        create();
-      }
+      console.log("MAILCHIMP ERROR", err);
+      callback(err);
     });
 };
